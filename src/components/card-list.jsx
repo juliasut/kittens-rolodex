be805af1e72ef24cd5d82bbb0ec488a0c3015d4d
iuliia sutygina <81769855+juliasut@users.noreply.github.com>
2021-10-17 13:22:03 -0600

    list Card components

--- src/components/card-list.jsx
+++ src/components/card-list.jsx
@@ -1,9 +1,12 @@
 import './card-list.css';
+import Card from './card/card';
 
 export default function CardList(props) {
   return (
     <div className="card-list">
-      {props.children}
+      {props.monsters.map((monster) => (
+          <Card key={monster.id} monster={monster}></ Card>
+        ))}
     </div>
   )
 }
