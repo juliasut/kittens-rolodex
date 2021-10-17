76235807f8eaa18535f6be014d21520b8d99df8d
iuliia sutygina <81769855+juliasut@users.noreply.github.com>
2021-10-17 11:37:38 -0600

    import CardList and pass monsters list as props

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import { Component } from 'react';
 import './App.css';
+import Cardlist from './components/card-list';
 
 class App extends Component {
   constructor() {
@@ -19,9 +20,11 @@ class App extends Component {
   render() {
     return (
       <div className="App">
+        <Cardlist>
         {this.state.monsters.map((monster) => (
           <h1 key={monster.id}>{monster.name}</h1>
         ))}
+        </Cardlist>
       </div>
     );
   }
