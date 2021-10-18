38134f225b6412016ed63029f22b4fcb671810d5
iuliia sutygina <81769855+juliasut@users.noreply.github.com>
2021-10-17 18:12:53 -0600

    add className attribute

--- src/components/search-box/search-box.jsx
+++ src/components/search-box/search-box.jsx
@@ -3,8 +3,8 @@ import './search-box.css';
 
 export default function SearchBox({placeholder, handleChange}) {
   return (
-    <div>
-      <input type="search" placeholder={placeholder} onChange={handleChange} />
+    <div >
+      <input className="search" type="search" placeholder={placeholder} onChange={handleChange} />
     </div>
   )
 }
